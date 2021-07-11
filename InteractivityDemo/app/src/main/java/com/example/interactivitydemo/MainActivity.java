70dfdccd071a33ee60b951467e8a97ee38e6550f
viveksoni100 <viveksoni100@gmail.com>
2021-07-11 14:31:33 +0530

    Login Demo project is added

--- InteractivityDemo/app/src/main/java/com/example/interactivitydemo/MainActivity.java
+++ InteractivityDemo/app/src/main/java/com/example/interactivitydemo/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.interactivitydemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view) {
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        Log.i("Info", "Welcome, " + nameEditText.getText().toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

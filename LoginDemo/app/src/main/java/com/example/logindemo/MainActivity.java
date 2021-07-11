181c54292bb72eda26ebd99eded9cc4d95716794
viveksoni100 <viveksoni100@gmail.com>
2021-07-11 15:02:51 +0530

    Image View is learned

--- LoginDemo/app/src/main/java/com/example/logindemo/MainActivity.java
+++ LoginDemo/app/src/main/java/com/example/logindemo/MainActivity.java
@@ -1,10 +1,24 @@
 package com.example.logindemo;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    public void login(View view) {
+        EditText usernameET = (EditText) findViewById(R.id.usernameET);
+        EditText passwordET = (EditText) findViewById(R.id.passwordET);
+        String usr = usernameET.getText().toString();
+        String pass = passwordET.getText().toString();
+        Log.i("Info", "Username : " + usr);
+        Log.i("Info", "Password : " + pass);
+        Toast.makeText(this, "You entered : " + usr + " " + pass , Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

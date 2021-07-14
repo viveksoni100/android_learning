456c946eec9e4977b7057b533dd8760dc19fa066
viveksoni100 <viveksoni100@gmail.com>
2021-07-14 06:14:46 +0530

    google map is added

---
+++ MapFun/app/src/main/java/com/example/mapfun/MapsActivity.java
@@ -0,0 +1,49 @@
+package com.example.mapfun;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
+        // Add a marker in Sydney and move the camera
+        LatLng vadtalMandir = new LatLng(22.592997991477123, 72.87365848126892);
+        mMap.addMarker(new MarkerOptions().position(vadtalMandir).title("શ્રી સ્વામિનારાયણ મંદિર, વડતાલ"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(vadtalMandir, 15));
+    }
+}

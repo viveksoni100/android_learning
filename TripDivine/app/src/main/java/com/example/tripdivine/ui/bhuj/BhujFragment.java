b35e9b872d198859b26385f62bd6f42afdeb53a5
viveksoni100 <viveksoni100@gmail.com>
2021-07-18 16:01:31 +0530

    .

---
+++ TripDivine/app/src/main/java/com/example/tripdivine/ui/bhuj/BhujFragment.java
@@ -0,0 +1,51 @@
+package com.example.tripdivine.ui.bhuj;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.tripdivine.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class BhujFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bhuj, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map_bhuj);
+        mapFragment.getMapAsync(this);
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        getBhujRegion(googleMap);
+    }
+
+    private void getBhujRegion(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng vadtalMandir = new LatLng(23.246523467835036, 69.66438152675865);
+        mMap.addMarker(new MarkerOptions().position(vadtalMandir).title("શ્રી સ્વામિનારાયણ મંદિર ભુજ"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(vadtalMandir, 15));
+    }
+}

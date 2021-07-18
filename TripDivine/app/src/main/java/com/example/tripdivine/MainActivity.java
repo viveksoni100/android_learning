25a451267e903fb97bb8a26bf12769bc44624afc
viveksoni100 <viveksoni100@gmail.com>
2021-07-18 14:44:01 +0530

    .

--- TripDivine/app/src/main/java/com/example/tripdivine/MainActivity.java
+++ TripDivine/app/src/main/java/com/example/tripdivine/MainActivity.java
@@ -43,9 +43,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         askForLocationPermission();
-
         setUpGoogleSignInOptions();
     }
 
--- TripDivine/app/src/main/java/com/example/tripdivine/ui/location/LocationFragment.java
+++ TripDivine/app/src/main/java/com/example/tripdivine/ui/location/LocationFragment.java
@@ -1,20 +1,18 @@
 package com.example.tripdivine.ui.location;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
@@ -50,7 +48,50 @@ public class LocationFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         // getVadtalMandirLocation(googleMap);
+        setupLocationListener(googleMap);
     }
+
+    private void setupLocationListener(GoogleMap googleMap) {
+        mMap = googleMap;
+        locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                mMap.clear();
+                LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(userLocation).title("Your Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        getUsersCurrentLocation();
+    }
+
+    private void getUsersCurrentLocation() {
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            mMap.clear();
+            LatLng userLocation = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(userLocation).title("Your last known location"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15));
+        }
+    }
+
 
     private void getVadtalMandirLocation(GoogleMap googleMap) {
         mMap = googleMap;

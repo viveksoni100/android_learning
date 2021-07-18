888acc31ff971b87454d422110534b5ae360d115
viveksoni100 <viveksoni100@gmail.com>
2021-07-18 11:20:54 +0530

    OAuth2 is implemented using GoogleApis

--- TripDivine/app/src/main/java/com/example/tripdivine/NotificationDrawer.java
+++ TripDivine/app/src/main/java/com/example/tripdivine/NotificationDrawer.java
@@ -1,11 +1,15 @@
 package com.example.tripdivine;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -14,23 +18,39 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
+import com.squareup.picasso.Picasso;
 
-public class NotificationDrawer extends AppCompatActivity {
+public class NotificationDrawer extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private AppBarConfiguration mAppBarConfiguration;
+    private NavigationView navigationView;
+
+    private GoogleApiClient googleApiClient;
+    private GoogleSignInOptions googleSignInOptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_drawer);
 
+        OAuth2SignInProcess();
+        InitNavigationDrawer();
+    }
+
+    private void InitNavigationDrawer() {
         Toolbar toolbar = findViewById(R.id.toolbar);
-
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,10 +59,8 @@ public class NotificationDrawer extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -50,21 +68,15 @@ public class NotificationDrawer extends AppCompatActivity {
                 R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
-
-        View headView = navigationView.getHeaderView(0);
-        ImageView imageView = headView.findViewById(R.id.imageView);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(NotificationDrawer.this, "Fuck me...!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-
         NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    private void OAuth2SignInProcess() {
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions).build();
     }
 
     @Override
@@ -79,5 +91,60 @@ public class NotificationDrawer extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(googleApiClient);
+        if (opr.isDone()) {
+            GoogleSignInResult result = opr.get();
+            handleSignInResults(result);
+        } else {
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult result) {
+                    handleSignInResults(result);
+                }
+            });
+        }
+    }
+
+    private void handleSignInResults(GoogleSignInResult result) {
+        Log.i("handling sign in results : ", "initiated...");
+        if (result.isSuccess()) {
+            Toast.makeText(this, "Signin successful", Toast.LENGTH_SHORT).show();
+            GoogleSignInAccount account = result.getSignInAccount();
+            View headView = navigationView.getHeaderView(0);
+
+            TextView displayName = headView.findViewById(R.id.displayName);
+            displayName.setText(account.getDisplayName());
+
+            TextView emailAddress = headView.findViewById(R.id.emailAddress);
+            emailAddress.setText(account.getEmail());
+
+            ImageView imageView = headView.findViewById(R.id.imageView);
+            Picasso.get().load(account.getPhotoUrl()).placeholder(R.mipmap.ic_launcher).into(imageView);
+            Log.i("handling sign in results : ", "completed...");
+        } else {
+            Log.i("handling sign in results : ", "failed...");
+            goToMainActivity();
+            Log.i("redirecting to main activity : ", "completed...");
+        }
+    }
+
+    private void goToMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }

d5153af04d02ff4ceffc2eb42935b4fec314675b
viveksoni100 <viveksoni100@gmail.com>
2021-09-20 11:18:52 +0530

    .

--- TripDivine/app/src/main/java/com/example/tripdivine/NotificationDrawer.java
+++ TripDivine/app/src/main/java/com/example/tripdivine/NotificationDrawer.java
@@ -69,8 +69,8 @@ public class NotificationDrawer extends AppCompatActivity implements GoogleApiCl
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_location, R.id.nav_ahmedabad /*R.id.nav_vadtal,
-                R.id.nav_bhuj, R.id.nav_feedback, R.id.nav_send*/)
+                R.id.nav_location, R.id.nav_ahmedabad, R.id.nav_bhuj
+                /*R.id.nav_vadtal, R.id.nav_feedback, R.id.nav_send*/)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
